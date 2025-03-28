43648a56451b163661caaa3196b033c347c610bc
hansfrugel21 <163870073+hansfrugel21@users.noreply.github.com>
2025-03-27 20:45:21 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
       2: new Date("2025-03-21T12:00:00"),
       3: new Date("2025-03-22T12:10:00"),
       4: new Date("2025-03-23T12:10:00"),
- 4: new Date("2025-03-27T12:10:00")
+ 5: new Date("2025-03-27T12:10:00")
     };
     const currentTime = new Date();
     const newGameStartedDays = {};
